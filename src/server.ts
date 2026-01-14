cb315dd7a403e626dd3632b113b9e34c7b634fe6
Aaron Adetunmbi <adetunmbikenny@gmail.com>
2026-01-14 20:46:31 +0100

    Cast process.env.PORT to number

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
